a5a5b3e9285a140aa22561c5bbad4a2e59afa0e6
brooks-coding <brookspiper04@gmail.com>
2025-12-02 17:18:11 -0800

    Add annotations

--- scripts/drafts/vignette-script.py
+++ scripts/drafts/vignette-script.py
@@ -1,10 +1,9 @@
+# Import dependencies
 import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
-
-
 
 # Get data
 url = 'https://raw.githubusercontent.com/pstat197/vignette-XGBoost/refs/heads/main/data/Iris.csv'
@@ -28,12 +27,12 @@ y = df["Species"]
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
 # Initialize and train the XGBoost classifier
-model = xgb.XGBClassifier(objective='multi:softmax', eval_metric='logloss')
+model = xgb.XGBClassifier(objective='multi:softmax', eval_metric='merror')
 model.fit(X_train, y_train)
 
 # Make predictions
 predictions = model.predict(X_test)
 
 # Evaluate the model
-accuracy = accuracy_score(y_test, predictions)
-print(f"Accuracy: {accuracy}")
+train_accuracy = accuracy_score(y_test, predictions)
+print(f"Accuracy: {train_accuracy}")
