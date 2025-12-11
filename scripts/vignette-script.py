b9437a08732d647577400950a00df567ce941bd9
brooks-coding <brookspiper04@gmail.com>
2025-12-10 20:28:29 -0800

    Add finalized script

--- scripts/vignette-script.py
+++ scripts/vignette-script.py
@@ -3,6 +3,9 @@ import xgboost as xgb
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
 
 # Get data from local CSV
 df = pd.read_csv("data/Iris.csv", index_col=0)
@@ -47,3 +50,30 @@ df_processed.loc[X_test.index, "Split"] = "test"
 
 # Save single processed CSV
 df_processed.to_csv("data/Iris_processed.csv", index=False)
+
+# Make predictions on test set
+predictions = model.predict(X_test)
+
+feature_importance = model.feature_importances_
+features = X.columns
+
+plt.figure(figsize=(8, 5))
+plt.barh(features, feature_importance)
+plt.xlabel('Importance')
+plt.title('Feature Importance in XGBoost Model')
+plt.tight_layout()
+plt.show()
+
+# Confusion matrix to see prediction performance by class
+
+cm = confusion_matrix(y_test, predictions)
+
+plt.figure(figsize=(7, 5))
+sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', 
+            xticklabels=['Setosa', 'Versicolor', 'Virginica'],
+            yticklabels=['Setosa', 'Versicolor', 'Virginica'])
+plt.xlabel('Predicted')
+plt.ylabel('Actual')
+plt.title('Confusion Matrix')
+plt.tight_layout()
+plt.show()
